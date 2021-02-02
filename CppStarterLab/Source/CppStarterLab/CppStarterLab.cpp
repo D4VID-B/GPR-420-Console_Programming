4c1012b44f59e22cb18189dc9db1f6d8dd2e939f
David Bakaleinik <david.bakaleinik@mymail.champlain.edu>
2021-02-02 21:54:45 +0100

    Adding first lab

---
+++ CppStarterLab/Source/CppStarterLab/CppStarterLab.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "CppStarterLab.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, CppStarterLab, "CppStarterLab" );
---
+++ CppStarterLab/Source/CppStarterLab/CppStarterLabGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "CppStarterLabGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CPPSTARTERLAB_API ACppStarterLabGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};

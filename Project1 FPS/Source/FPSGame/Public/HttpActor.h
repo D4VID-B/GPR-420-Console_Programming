f8904a1be7e4c6f53808caa68a258015f12c1a84
Benjamin Cooper <benjamin.cooper@mymail.champlain.edu>
2021-03-09 15:23:25 -0500

    In class tutorial completed. Working on weather data next

---
+++ Project1 FPS/Source/FPSGame/Public/HttpActor.h
@@ -0,0 +1,34 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Runtime/Online/HTTP/Public/Http.h"
+#include "HttpActor.generated.h"
+
+UCLASS()
+class FPSGAME_API AHttpActor : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AHttpActor();
+	FHttpModule* Http;
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+	UPROPERTY(EditDefaultsOnly, Category = "API")
+	float delay;
+public:
+	/*making the api request*/
+	UFUNCTION()
+	void MyHttpCall();
+	/*processing the GET request*/
+	void OnResponseReceived(FHttpRequestPtr Request, FHttpResponsePtr Response, bool bWasSuccessful);
+
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+};

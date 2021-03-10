320ac619bae4013116f42792250db15c8626f668
Benjamin Cooper <benjamin.cooper@mymail.champlain.edu>
2021-03-10 18:07:09 -0500

    Weather API works

--- Project1 FPS/Source/FPSGame/Private/HttpActor.cpp
+++ Project1 FPS/Source/FPSGame/Private/HttpActor.cpp
@@ -45,17 +45,42 @@ void AHttpActor::OnResponseReceived(FHttpRequestPtr Request, FHttpResponsePtr Re
 
 	//Create a reader pointer to read the json data
 	TSharedRef<TJsonReader<>> Reader = TJsonReaderFactory<>::Create(Response->GetContentAsString());
-
 	//Deserialize the json data given Reader and the actual object to deserialize
 	if (FJsonSerializer::Deserialize(Reader, JsonObject))
 	{
+		TSharedPtr<FJsonValue, ESPMode::NotThreadSafe> obj = JsonObject->TryGetField("properties");
 		//Get the value of the json object by field name
-		TArray<TSharedPtr<FJsonValue>> objArray = JsonObject->GetArrayField("periods");
+		TArray<TSharedPtr<FJsonValue>> objArray = obj->AsObject()->GetArrayField("periods");
 		for (int32 i = 0; i < objArray.Num(); i++)
 		{
-			FString artist = objArray[i]->AsObject()->GetStringField(TEXT("temperature"));
-			GEngine->AddOnScreenDebugMessage(1 + i, 2.0f, FColor::Green, artist);
+			FString temperature = objArray[i]->AsObject()->GetStringField(TEXT("temperature"));
+			GEngine->AddOnScreenDebugMessage(1 + i, 2.0f, FColor::Green, temperature);
+			FString windDirection = objArray[i]->AsObject()->GetStringField(TEXT("windDirection"));
+			GEngine->AddOnScreenDebugMessage(14 + i, 2.0f, FColor::Red, windDirection);
 		}
+
+		
+		
+		/*if (JsonObject->TryGetField("properties"))
+		{
+			GEngine->AddOnScreenDebugMessage(1, 2.0f, FColor::Green, "Periods found");
+		}
+		else
+		{
+			GEngine->AddOnScreenDebugMessage(1, 2.0f, FColor::Green, "Periods not found");
+		}*/
+		
+			/*TArray<TSharedPtr<FJsonValue>> objArray2 = JsonObject->GetArrayField("periods");
+			for (int32 j = 0; j < objArray.Num(); j++)
+			{
+				FString temperature = objArray2[j]->AsObject()->GetStringField(TEXT("temperature"));
+				GEngine->AddOnScreenDebugMessage(1 + j, 2.0f, FColor::Green, temperature);
+			}*/
+			
+		//{
+
+		//}
+		/**/
 		//int32 recievedInt = JsonObject->GetIntegerField(objArray[0]->AsString());
 
 		//Output it to the engine
